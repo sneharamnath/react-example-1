d6b85c0dacf52ffe985bbb5f6cb7c012b66ee533
snehaRCanvera <sneha.ramnath@canvera.com>
2016-09-09 17:58:50 +0530

    First commit

--- js/components/App.js
+++ js/components/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
 
+class Header extends React.Component {
+    render () {
+        return (
+            <header>
+                <h1>{this.props.title}</h1>
+            </header>
+        );
+    }
+};
+
+class CandidateList extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			data : {} 
+		}
+	}
+  	loadCommentsFromServer () {
+	    $.ajax({
+	      url: this.props.url,
+	      dataType: 'json',
+	      cache: false,
+	      success: function(data) {
+	        this.setState({data: data});
+	      }.bind(this),
+	      error: function(xhr, status, err) {
+	        console.error(this.props.url, status, err.toString());
+	      }.bind(this)
+	    });
+  	}
+  	componentDidMount () {
+    	this.loadCommentsFromServer();
+  	}
+  	render () { 
+  		return (
+  			<CandidateItem data={this.state.data.candidates} />
+  		)	
+  	}
+}
+
+class CandidateItem extends React.Component {
+    render () {
+        // var items = this.props.data.map(function (item) {    
+	        return (
+	        	<div>
+	        		Test
+	        	</div>
+	            // <ul>
+	            //     <li> Component 1 </li>
+	            //     <li> Component 2 </li>
+	            //     <li> Component 3 </li>
+	            // </ul>
+	        );
+        // });
+    }
+};
+
 class App extends React.Component {
 	render() {
 		return (
-			<div>Hello World</div>
+			<Header title="Candidate List" />,
+			<CandidateList url="http://104.199.147.85/candidates" />
 		)
 	}
 }
-
 export default App
